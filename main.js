8694d195e2e9ba1ff4f2b6e969f90e5ed81ebf9e
Ashu-less <ashu.brant@gmail.com>
2025-01-13 21:52:09 -0600

    js fixes for ui, need to get recipes table and then connection to homepage

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 document.addEventListener("DOMContentLoaded", () =>
     {
         const signInButton = document.getElementById("signInButton");
@@ -10,3 +11,31 @@ document.addEventListener("DOMContentLoaded", () =>
     } 
 
 )
+=======
+
+
+document.addEventListener('DOMContentLoaded', () => {
+    const signInBtn = document.getElementById('SignInBtn');
+    const signUpBtn = document.getElementById('SignUpBtn');
+    const signInForm = document.getElementById('signInForm');
+    const signUpForm = document.getElementById('signUpForm');
+
+    signInForm.style.display = 'block';
+    signUpForm.style.display = 'none';
+
+    signInBtn.addEventListener('click', () => {
+        signInForm.style.display = 'block';
+        signUpForm.style.display = 'none';
+        signInBtn.classList.add('active');
+        signUpBtn.classList.remove('active');
+
+    });
+    signUpBtn.addEventListener('click', () => {
+        signUpForm.style.display = 'block';
+        signInForm.style.display = 'none';
+        signUpBtn.classList.add('active');
+        signInBtn.classList.remove('active');
+    });
+
+});
+>>>>>>> Stashed changes
